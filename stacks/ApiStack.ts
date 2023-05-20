6732dd163e4dc27bd58043f6e65c3b17e4f62ccf
Roger Rajaratnam <roger@sourcier.uk>
2023-05-20 01:58:11 +0100

    Add an API to list all the notes

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -14,6 +14,7 @@ export function ApiStack({ stack }: StackContext) {
     routes: {
       "POST /notes": "packages/functions/src/create.main",
       "GET /notes/{id}": "packages/functions/src/get.main",
+      "GET /notes": "packages/functions/src/list.main",
     },
   });
 
