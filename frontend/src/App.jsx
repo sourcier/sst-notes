c00cc2021c2d24ff4df6d66f88e00a343dd193f5
Roger Rajaratnam <roger@sourcier.uk>
2023-05-30 00:48:47 +0100

    create containers

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import "./App.css";
+import Routes from "./Routes";
+
+function App() {
+  return (
+    <div className="App container py-3">
+      <Navbar collapseOnSelect bg="light" expand="md" className="mb-3 px-3">
+        <Navbar.Brand className="fw-bold text-muted">Scratch</Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar>
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
