880c4d4ddfca073bcfee53b2ff979c2aa4b9b3dc
Roger Rajaratnam <roger@sourcier.uk>
2023-06-01 13:14:28 +0100

    create a settings page

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -7,6 +7,7 @@ import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import NewNote from "./containers/NewNote";
 import Notes from "./containers/Notes";
+import Settings from "./containers/Settings";
 
 export default function Links() {
   return (
@@ -16,6 +17,7 @@ export default function Links() {
       <Route path="/signup" element={<Signup />} />
       <Route path="/notes/new" element={<NewNote />} />
       <Route path="/notes/:id" element={<Notes />} />
+      <Route path="/settings" element={<Settings />} />
       {/* Finally, catch all unmatched routes */}
       <Route path="*" element={<NotFound />} />;
     </Routes>
