6cec210cdbbc774f9314f4e14200b72f36e75b26
Roger Rajaratnam <roger@sourcier.uk>
2023-06-01 12:20:59 +0100

    add the create note page

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -5,6 +5,7 @@ import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
 
 export default function Links() {
   return (
@@ -12,6 +13,7 @@ export default function Links() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/notes/new" element={<NewNote />} />
       {/* Finally, catch all unmatched routes */}
       <Route path="*" element={<NotFound />} />;
     </Routes>
