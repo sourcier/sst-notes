98951c8fc0f774b542d311125ee292c386caf8e3
Roger Rajaratnam <roger@sourcier.uk>
2023-07-11 00:40:51 +0100

    report API errors in React

--- frontend/src/lib/errorLib.js
+++ frontend/src/lib/errorLib.js
@@ -21,3 +21,22 @@ export function logError(error, errorInfo = null) {
     Sentry.captureException(error);
   });
 }
+
+export function onError(error) {
+  let errorInfo = {};
+  let message = error.toString();
+
+  // Auth errors
+  if (!(error instanceof Error) && error.message) {
+    errorInfo = error;
+    message = error.message;
+    error = new Error(message);
+    // API errors
+  } else if (error.config && error.config.url) {
+    errorInfo.url = error.config.url;
+  }
+
+  logError(error, errorInfo);
+
+  alert(message);
+}
