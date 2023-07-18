3c9610344116a366dadf35b56bbcbe3628967f5c
Roger Rajaratnam <roger@sourcier.uk>
2023-07-18 14:24:17 +0100

    Reverting faulty code

--- packages/functions/src/get.js
+++ packages/functions/src/get.js
@@ -1,21 +1,18 @@
 import { Table } from "sst/node/table";
-import handler from "@sst-notes/core/handler";
-import dynamoDb from "@sst-notes/core/dynamodb";
+import handler from "@notes/core/handler";
+import dynamoDb from "@notes/core/dynamodb";
 
-// Wrong handler function name
-export const main2 = handler(async (event) => {
+export const main = handler(async (event) => {
   const params = {
     TableName: Table.Notes.tableName,
     // 'Key' defines the partition key and sort key of the item to be retrieved
-    // - 'userId': Identity Pool identity id of the authenticated user
-    // - 'noteId': path parameter
     Key: {
       userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId,
-      noteId: event.pathParameters.id,
+      noteId: event.pathParameters.id, // The id of the note from the path
     },
   };
 
-  const result = await dynamoDbLib.call("get", params);
+  const result = await dynamoDb.get(params);
   if (!result.Item) {
     throw new Error("Item not found.");
   }
