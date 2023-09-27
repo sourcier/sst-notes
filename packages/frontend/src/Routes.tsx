259e35fa3c8ebd3dfe929a8d720b7017eecde6b7
Roger Rajaratnam <roger@sourcier.uk>
2023-09-27 23:42:48 +0100

    Create a login page

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 
 import Home from "./containers/Home.tsx";
+import Login from "./containers/Login.tsx";
 import NotFound from "./containers/NotFound.tsx";
 
 export default function Links() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
       {/* Finally, catch all unmatched routes */}
       <Route path="*" element={<NotFound />} />;
     </Routes>
