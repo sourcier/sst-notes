56491ff54caa77bc0894644364bc3b3d34116b3d
Roger Rajaratnam <roger@sourcier.uk>
2023-09-27 23:35:46 +0100

    Adding links in the navbar

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { LinkContainer } from "react-router-bootstrap";
 
 import Routes from "./Routes.tsx";
 import "./App.css";
@@ -7,11 +9,24 @@ function App() {
   return (
     <div className="App container py-3">
       <Navbar collapseOnSelect bg="light" expand="md" className="mb-3 px-3">
-        <Navbar.Brand className="fw-bold text-muted">Scratch</Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand className="fw-bold text-muted">Scratch</Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav activeKey={window.location.pathname}>
+            <LinkContainer to="/signup">
+              <Nav.Link>Signup</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
       <Routes />
     </div>
   );
 }
+
 export default App;
