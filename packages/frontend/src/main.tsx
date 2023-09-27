536938a7581328635c3badf8c23aba0107da73e5
Roger Rajaratnam <roger@sourcier.uk>
2023-09-27 23:29:46 +0100

    Handle routes with React Router

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import App from './App.tsx'
-import './index.css'
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
