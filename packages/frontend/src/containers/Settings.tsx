5d190dad4ac3426a3c3e28f852598ea3602e49b5
Roger Rajaratnam <roger@sourcier.uk>
2023-09-28 21:45:56 +0100

    Create a settings page

---
+++ packages/frontend/src/containers/Settings.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import config from "../config";
+import { API } from "aws-amplify";
+import { onError } from "../lib/errorLib";
+import { useNavigate } from "react-router-dom";
+import { BillingType } from "../types/billing";
+
+export default function Settings() {
+  const nav = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+
+  function billUser(details: BillingType) {
+    return API.post("notes", "/billing", {
+      body: details,
+    });
+  }
+
+  return <div className="Settings"></div>;
+}
