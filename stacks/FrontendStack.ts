74145effe97794ed9d5273f58fed571673421cfc
Roger Rajaratnam <roger@sourcier.uk>
2023-09-28 22:14:21 +0100

    remove cra frontend app

--- stacks/FrontendStack.ts
+++ stacks/FrontendStack.ts
@@ -8,22 +8,6 @@ export function FrontendStack({ stack, app }: StackContext) {
   const { auth } = use(AuthStack);
   const { bucket } = use(StorageStack);
 
-  // CRA app
-  // const site = new StaticSite(stack, "ReactSite", {
-  //   path: "frontend-cra",
-  //   buildOutput: "build",
-  //   buildCommand: "npm run build",
-  //   // Pass in our environment variables
-  //   environment: {
-  //     REACT_APP_API_URL: api.customDomainUrl || api.url,
-  //     REACT_APP_REGION: app.region,
-  //     REACT_APP_BUCKET: bucket.bucketName,
-  //     REACT_APP_USER_POOL_ID: auth.userPoolId,
-  //     REACT_APP_IDENTITY_POOL_ID: auth.cognitoIdentityPoolId || "",
-  //     REACT_APP_USER_POOL_CLIENT_ID: auth.userPoolClientId,
-  //   },
-  // });
-
   // Vite app
   const site = new StaticSite(stack, "ReactSite", {
     path: "packages/frontend",
