b596f9951f674da4ea792495ce1c00d1277a03c9
Roger Rajaratnam <roger@sourcier.uk>
2023-09-28 21:34:09 +0100

    Call the list API

--- packages/frontend/src/containers/Home.tsx
+++ packages/frontend/src/containers/Home.tsx
@@ -1,15 +1,68 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ListGroup from "react-bootstrap/ListGroup";
+import { BsPencilSquare } from "react-icons/bs";
+import { LinkContainer } from "react-router-bootstrap";
+import { API } from "aws-amplify";
+
+import { NoteType } from "../types/note";
+import { onError } from "../lib/errorLib";
 import { useAppContext } from "../lib/contextLib";
 import "./Home.css";
 
 export default function Home() {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState<Array<NoteType>>([]);
   const { isAuthenticated } = useAppContext();
   const [isLoading, setIsLoading] = useState(true);
 
-  function renderNotesList(notes: { [key: string | symbol]: any }) {
-    return null;
+  useEffect(() => {
+    async function onLoad() {
+      if (!isAuthenticated) {
+        return;
+      }
+
+      try {
+        const notes = await loadNotes();
+        setNotes(notes);
+      } catch (e) {
+        onError(e);
+      }
+
+      setIsLoading(false);
+    }
+
+    onLoad();
+  }, [isAuthenticated]);
+
+  function loadNotes() {
+    return API.get("notes", "/notes", {});
+  }
+
+  function formatDate(str: undefined | string) {
+    return !str ? "" : new Date(str).toLocaleString();
+  }
+
+  function renderNotesList(notes: NoteType[]) {
+    return (
+      <>
+        <LinkContainer to="/notes/new">
+          <ListGroup.Item action className="py-3 text-nowrap text-truncate">
+            <BsPencilSquare size={17} />
+            <span className="ms-2 fw-bold">Create a new note</span>
+          </ListGroup.Item>
+        </LinkContainer>
+        {notes.map(({ noteId, content, createdAt }) => (
+          <LinkContainer key={noteId} to={`/notes/${noteId}`}>
+            <ListGroup.Item action className="text-nowrap text-truncate">
+              <span className="fw-bold">{content.trim().split("\n")[0]}</span>
+              <br />
+              <span className="text-muted">
+                Created: {formatDate(createdAt)}
+              </span>
+            </ListGroup.Item>
+          </LinkContainer>
+        ))}
+      </>
+    );
   }
 
   function renderLander() {
