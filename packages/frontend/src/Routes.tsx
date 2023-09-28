02c9210523c85b8e8c7a4d781ba27f67f733b172
Roger Rajaratnam <roger@sourcier.uk>
2023-09-28 21:26:10 +0100

    Add the create note page

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./containers/Home.tsx";
 import Login from "./containers/Login.tsx";
 import Signup from "./containers/Signup.tsx";
+import NewNote from "./containers/NewNote.tsx";
 import NotFound from "./containers/NotFound.tsx";
 
 export default function Links() {
@@ -11,6 +12,7 @@ export default function Links() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/notes/new" element={<NewNote />} />
       {/* Finally, catch all unmatched routes */}
       <Route path="*" element={<NotFound />} />;
     </Routes>
